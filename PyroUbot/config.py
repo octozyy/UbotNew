f337f81f07e05de2b4cbcaf19ff6f08b22ceacc5
Octozyy Olympus <rifkipradana014@gmail.com>
2025-10-19 20:56:35 +0700

    Add files via upload

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -1,1 +1,24 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv(".env")
+
+MAX_BOT = int(os.getenv("MAX_BOT", "100"))
+
+DEVS = list(map(int, os.getenv("DEVS", "6615675254").split()))
+
+API_ID = int(os.getenv("API_ID", "23226782"))
+
+API_HASH = os.getenv("API_HASH", "50a8a9ca1b1bd90c81f9acc36da2f511")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8482482651:AAGyR55eduojEZNgWWufJoatZgB_1U6F_Hs")
+
+OWNER_ID = int(os.getenv("OWNER_ID", "6615675254"))
+
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763").split()))
+
+RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://ibnumuzakim7:ibnumuzakim132@ibnumuzakim.sbwnig8.mongodb.net/")
+
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-4912568273"))
